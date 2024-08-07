a6d9ca10ddaf6d1af7f228bb2d4e55a952b5e08e
Harsh <harshsik7890@gmail.com>
2024-08-08 02:01:05 +0530

    created collection ui and functionality

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,63 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import FilterComponent from "@/components/shared/FilterComponent";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { QuestionFilters } from "@/contants/Filters";
+import { getSavedQuestion } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const { userId } = auth();
+  if (!userId) return null;
+  const result = await getSavedQuestion({ clerkId: userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 sm:text-2xl">
+        Saved Questions
+      </h1>
+
+      <div className="mt-11 flex justify-between gap-4 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <FilterComponent
+          filters={QuestionFilters}
+          otherClasses="mih-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result?.questions.length > 0 ? (
+          result?.questions.map((quesiton: any) => (
+            <QuestionCard
+              key={quesiton._id}
+              _id={quesiton._id}
+              title={quesiton.title}
+              tags={quesiton.tags}
+              author={quesiton.author}
+              upvotes={quesiton.upvotes}
+              views={quesiton.views}
+              answers={quesiton.answers}
+              createdAt={quesiton.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no saved question to show"
+            description="Be the first break the silence! 🚀 Ask a Question and kickstart the
+        discussion. Our query could be the next big thing others learn from =.
+        Get Involved! 💡"
+            link="/"
+            linkTitle=" Save a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,13 +6,15 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   GetUserByIdParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./share.types";
+import { FilterQuery } from "mongoose";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.mode";
-import { IM_Fell_English } from "next/font/google";
+import Tag from "@/database/tag.model";
 
 export async function getUserById(params: GetUserByIdParams) {
   try {
@@ -107,4 +109,29 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
   } catch (error) {
     throw error;
   }
+}
+
+export async function getSavedQuestion(params: GetSavedQuestionsParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: { sort: { createdAt: -1 } },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!user) throw new Error("user not found");
+
+    const savedQuestions = user.saved;
+    return { questions: savedQuestions };
+  } catch (error) {}
 }
